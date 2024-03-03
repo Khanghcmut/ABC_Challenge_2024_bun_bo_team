78a07e89b1952e1b033e2392e87382e60d74123d
BUI TRUONG VI <49474873+Buitruongvi@users.noreply.github.com>
2024-03-03 21:58:37 +0700

    feeder for GCN

--- GCN/main.py
+++ GCN/main.py
@@ -1,6 +1,5 @@
 from GCN.feeder import feeder_ABC
 
-
 root_path = '/Users/vibuitruong/Documents/GitHub/ABC_Challenge_2024_bun_bo_team/data/Dataset-2'
 dataset = feeder_ABC.Feeder(root_path)
 for data, label, index in (dataset):
